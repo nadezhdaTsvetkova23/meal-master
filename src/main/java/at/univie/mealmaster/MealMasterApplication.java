2ccab9202bbd76dfa2596cb3ba29dc6874db4047
Lucas Enzi <lucas.enzi@icloud.com>
2023-12-30 17:44:35 +0100

    initial commit

---
+++ src/main/java/at/univie/mealmaster/MealMasterApplication.java
@@ -0,0 +1,13 @@
+package at.univie.mealmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/univie/mealmaster/MealMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package at.univie.mealmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
