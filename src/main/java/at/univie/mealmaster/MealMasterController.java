8f4507964abb7fea007cb34c71f44fd37396a4f4
Lucas Enzi <lucas.enzi@icloud.com>
2024-01-15 19:19:55 +0100

    Updated to show Generator only on first visit

--- src/main/java/at/univie/mealmaster/MealMasterController.java
+++ src/main/java/at/univie/mealmaster/MealMasterController.java
@@ -339,7 +339,7 @@ public class MealMasterController {
         recipeRepository.saveAll(mg.getRecipes());
         recipeIngredientRepository.saveAll(mg.getRecipeIngredients());
 
-        //add generateData()
+        new CheckIfContentGenerated().writeTrueToFile();
         return "redirect:/";
     }
 }
